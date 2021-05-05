885184f3ecf99b7cdb963dcbdf13ea41f3e33fba
Tadam1999 <toth99adam@gmail.com>
2021-05-05 16:41:54 +0200

    Code review 1.0
    Kész, elkészült az összes osztály átrendezése, valamint az eseménykezelés. Elvileg csak a Gamben hiányoznak getterek, azokra dob errort.

--- program/Skeleton/src/main/DialogSelect3.java
+++ program/Skeleton/src/main/DialogSelect3.java
@@ -1,31 +1,86 @@
 package main;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  * Azon párbeszédablakok absztrakt õse, ahol a felhasználó hérom paramétert ad meg a mûvelet számára.
  * @author Totya
  */
-public abstract class DialogSelect3 extends JDialog {
+public class DialogSelect3 extends JDialog {
 	protected Game game;
 	protected Controller controller;
 	protected JComboBox<String> comboTop;
 	protected JComboBox<String> comboMiddle;
 	protected JComboBox<String> comboBottom;
 	protected JButton bSelect;
+	protected JLabel labelTitle;
 	protected JLabel labelTop;
 	protected JLabel labelMiddle;
 	protected JLabel labelBottom;
 	
-	/**
-	 * A kiválasztás gomb eseménykezelõje.
-	 * @param e
-	 */
-	public abstract void selectAction(ActionEvent e);
+	public DialogSelect3(Game g, Controller cont) {
+		game = g;
+		controller = cont;
+		JPanel panel = new JPanel(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridheight = 8;
+		c.gridwidth = 3;
+		
+		//Cím label lehelyezése
+		c.gridy = 0;
+		c.gridx = 1;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		labelTitle = new JLabel();
+		panel.add(labelTitle, c);
+		
+		//Elsõ label lehelyezése
+		c.gridy = 1;
+		c.gridx = 0;
+		labelTop = new JLabel();
+		panel.add(labelTop, c);
+		
+		//Elsõ combobox lehelyezése
+		c.gridy = 2;
+		comboTop = new JComboBox<String>();
+		panel.add(comboTop, c);
+		
+		//Második label elhelyezése.
+		c.gridy = 3;
+		this.labelMiddle = new JLabel();
+		panel.add(labelMiddle, c);
+		
+		//Alsó comboBox lehelyezése.
+		c.gridy = 4;
+		this.comboMiddle = new JComboBox<String>();
+		panel.add(comboMiddle, c);
+		
+		//Második label elhelyezése.
+		c.gridy = 5;
+		this.labelBottom = new JLabel();
+		panel.add(labelBottom, c);
+		
+		//Alsó comboBox lehelyezése.
+		c.gridy = 6;
+		this.comboBottom = new JComboBox<String>();
+		panel.add(comboBottom, c);
+		
+		//Select gomb elhelyezése.
+		c.gridy = 7;
+		c.gridx = 1;
+		c.fill = GridBagConstraints.NONE;
+		this.bSelect = new JButton("Select");
+		panel.add(bSelect, c);
+		
+		//Panel elhelyezése a dialógusablakon.
+		this.add(panel);
+	}
 
 }
--- program/Skeleton/src/main/MoveDialog.java
+++ program/Skeleton/src/main/MoveDialog.java
@@ -3,6 +3,7 @@ package main;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
@@ -18,90 +19,144 @@ import javax.swing.JPanel;
  */
 public class MoveDialog extends DialogSelect3 {
 	
+	/**
+	 * A mozgás dialógusablalkának konstruktora.
+	 * @param g: Game: A játék, aki a szükséges adatokat tárolja.
+	 * @param cont: Controller: A kontroller, aki vezérli a mozgást.
+	 */
 	public MoveDialog(Game g, Controller cont) {
-		game = g;
-		controller = cont;
-		this.comboMiddle = new JComboBox<String>();
-		this.comboBottom = new JComboBox<String>();
-		JPanel panel = new JPanel(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
-		c.gridheight = 8;
-		c.gridwidth = 3;
-		
-		//Címet kijelölõ label.
-		c.gridy = 0;
-		c.gridx = 1;
-		JLabel titleLabel = new JLabel("Move with Settler");
-		panel.add(titleLabel, c);
-		
-		//Elsõ combobox label-je
-		c.gridy = 1;
-		c.gridx = 0;
-		c.fill = GridBagConstraints.HORIZONTAL;
-		JLabel firstLabel = new JLabel("Destination Type:");
-		panel.add(firstLabel, c);
-		
-		//Elsõ combobox
-		c.gridy = 2;
+		super(g, cont);		
+		this.labelTitle.setText("Move with Settler");
+		this.labelTop.setText("Destination Type:");
 		String[] destTypes = new String[] {"Asteroid", "StarGate"};
-		this.comboTop = new JComboBox<String>(destTypes);
-		this.comboTop.addItemListener(new ItemListener() {
-			public void itemStateChanged(ItemEvent e) {
-				JComboBox<String> combo = (JComboBox<String>) e.getSource();
-				String selectedType = (String) combo.getSelectedItem();
-				//Itt a kiválasztott célnak megfelelõen feltöltöm a középsõ comboboxot.
-				if(selectedType == "Asteroid") {
-					this.comboMiddle.removeAllItems();
-					String[] ids = game.getAsteroidIds();
-					for(int i = 0; i < ids.length; i++) {
-						this.comboMiddle.addItem(ids[i]);
-					}
-				}
-				else if(selectedType == "StarGate") {
-					this.comboMiddle.removeAllItems();
-					String[] ids = game.getStarGateIds();
-					for(int i = 0; i < ids.length; i++) {
-						this.comboMiddle.addItem(ids[i]);
-					}
-				}
-			}
-		});
-		panel.add(this.comboTop, c);
-		
-		//Második combobox label-je
-		c.gridy = 3;
-		JLabel secondLabel = new JLabel("Id:");
-		panel.add(secondLabel, c);
-		
-		//Második combobox
-		c.gridy = 4;
-		panel.add(comboMiddle, c);
-		
-		//Harmadik combobox label-je
-		c.gridy = 5;
-		JLabel thirdLabel = new JLabel("Settler Id:");
-		panel.add(thirdLabel, c);
-		
-		//Harmadik combobox
-		c.gridy = 6;
-		this.comboBottom = new JComboBox<String>(game.getSettlerIDs());
-		panel.add(this.comboBottom, c);
-		
-		//Select gomb elhelyezése.
-		c.gridy = 7;
-		c.gridx = 1;
-		c.fill = GridBagConstraints.NONE;
-		this.bSelect = new JButton("Select");
-		//Ez itt nem lesz jó, kell egy saját actionListener osztály.
-		//De itt lenne az eseménykezelés.
-		this.bSelect.addActionListener(selectAction);
-		panel.add(this.bSelect, c);	
+		for(int i = 0; i < destTypes.length; i++) {
+			this.comboTop.addItem(destTypes[i]);
+		}
+		this.labelMiddle.setText("Id:");
+		this.labelBottom.setText("Settler Id:");
+		SettlerSelectListener settlerListener = new SettlerSelectListener(this.game, this.comboTop, this.comboMiddle);
+		this.comboBottom.addItemListener(settlerListener);
+		MoveListener moveListener = new MoveListener(this, this.controller, this.comboTop, this.comboMiddle, this.comboBottom);
+		this.bSelect.addActionListener(moveListener);
 	}
 	
-	//Select gomb lenyomása.
-	public void selectAction(ActionEvent e) {
-		controller.handleMove((String) this.comboBottom.getSelectedItem(), (String) this.comboMiddle.getSelectedItem(), (String) this.comboTop.getSelectedItem());
-		this.setVisible(false);		
+	/**
+	 * Dialógusablak megjelenítésekor levesszük az eddigi telepeseket, majd feltöltjük az aktuális adatokkal az alsó dobozt.
+	 */
+	public void show() {
+		this.comboBottom.removeAllItems();
+		this.comboMiddle.removeAllItems();
+		String[] settlerIDs = game.getSettlerIds();
+		for(int i = 0; i < settlerIDs.length; i++) {
+			this.comboBottom.addItem(settlerIDs[i]);
+		}
 	}
+	
+	/**
+	 * A telepest választó combobox eseménykezelõ osztálya.
+	 * A mozgást kezelõ Dialógusablak belsõ osztálya.
+	 * @author Totya
+	 */
+	private class SettlerSelectListener implements ItemListener{
+		private Game game;
+		private JComboBox<String> comboDestType;
+		private JComboBox<String> comboDestID;
+		
+		/**
+		 * A telepes választó combobox eseménykezelõjének konstruktora.
+		 * @param g: Game: A játék, akitõl lekérhetem az aktuális választható adatokat.
+		 * @param cdesttype: JComboBox<String>: A ComboBox, ami tartalmazza az úticél típusát.
+		 * @param cdestid: JComboBox<String>: A ComboBox, ami majd tartalmazza a választhaó célok azonosítóit.
+		 */
+		public SettlerSelectListener(Game g, JComboBox<String> cdesttype, JComboBox<String> cdestid) {
+			game = g;
+			comboDestType = cdesttype;
+			comboDestID = cdestid;
+		}
+		
+		/**
+		 * Telepes választása esetén fut le.
+		 * A választott típustól függõen feltölti az úticélok dobozát a lehetséges azonosítókkal.
+		 */
+		public void itemStateChanged(ItemEvent e) {
+			comboDestID.removeAllItems();
+			JComboBox<String> src = (JComboBox<String>) e.getSource();
+			String settlerID = (String) src.getSelectedItem();
+			String selectedDestType = (String) comboDestType.getSelectedItem();
+			//Ha nincs kiválasztott uticéltípus, visszatérünk.
+			if(selectedDestType == null || selectedDestType == "") {
+				return;
+			}
+			//Ha nincs kiválaszott telepes visszatérünk.
+			if(settlerID == null || settlerID == "") {
+				return;
+			}
+			//Megnézzük milyen úticél van kiválasztva, majd hogy hová léphet a kiválasztott telepes.
+			if(selectedDestType == "Asteroid") {
+				String[] destIDs = game.getSettlersNeighboursID(settlerID);
+				for(int i = 0; i < destIDs.length; i++) {
+					comboDestID.addItem(destIDs[i]);
+				}
+			}
+			else if(selectedDestType == "StarGate") {
+				String[] destIDs = game.getSettlersStargatesID(settlerID);
+				for(int i = 0; i < destIDs.length; i++) {
+					comboDestID.addItem(destIDs[i]);
+				}
+			}			
+		}		
+	}
+	
+	/**
+	 * A mozgást elindító gomb eseménykezelõ osztálya.
+	 * A mozgást kezelõ Dialógusablak belsõ osztálya.
+	 * @author Totya
+	 */
+	private class MoveListener implements ActionListener {
+		private DialogSelect3 parentDialog;
+		private Controller controller;
+		private JComboBox<String> comboDestType;
+		private JComboBox<String> comboDestID;
+		private JComboBox<String> comboSettlerID;
+		
+		/**
+		 * A mozgást elindító gomb eseménykezelõ osztályának konstruktora.
+		 * @param dialog: DialogSelect3: A dialógusablak, amit a végén bekéne zárni.
+		 * @param cont: Controller: A mozgást végrehajtó kontroller.
+		 * @param destType: JComboBox<String>: A mozgás céljának típusát tartalmazó combobox.
+		 * @param destID: JComboBox<String>: Az úticél azonosítóját tartalmazó combobox.
+		 * @param settlerID: JComboBox<String>: A mozgó telepes azonosítóját tartalmazó combobox.
+		 */
+		public MoveListener(DialogSelect3 dial, Controller cont, JComboBox<String> destType, JComboBox<String> destID, JComboBox<String> settlerID) {
+			parentDialog = dial;
+			controller = cont;
+			comboDestType = destType;
+			comboDestID = destID;
+			comboSettlerID = settlerID;
+		}
+		/**
+		 * A mozgást végrehajtó eseménykezelés.
+		 * A végén bezárja a dialógusablakot.
+		 */
+		public void actionPerformed(ActionEvent e) {
+			String destType = (String) comboDestType.getSelectedItem();
+			String destID = (String) comboDestID.getSelectedItem();
+			String settlerID = (String) comboSettlerID.getSelectedItem();
+			//Ha bármelyi kiválasztott paraméter üres lenne, visszatérünk.
+			if (destType == null || destType == "") {
+				return;
+			}
+			if (destID == null || destID == "") {
+				return;
+			}
+			if (settlerID == null || settlerID == "") {
+				return;
+			}
+			controller.handleMove(settlerID, destType, destID);
+			parentDialog.setVisible(false);
+		}
+		
+	}
+	
 
 }
